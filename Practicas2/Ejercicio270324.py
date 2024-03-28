672ee070cc6972b0dcc7ee0d22b171c3c806d627
Endorphyne <ohorodeski@gmail>
2024-03-28 14:47:05 -0300

    update

--- Practicas2/Ejercicio270324.py
+++ Practicas2/Ejercicio270324.py
@@ -9,16 +9,10 @@
 #     return cantidadVocales
 # print(contadorVocales())
 
-#Crear una funcion que actualice el salario de los empleados tomando como valores un diccionario de empleados(empleado:sueldo) y que le suba el salario en un 30% 
+#Crear una funcion que actualice el salario de los empleados tomando como valores un diccionario de empleados(empleado:sueldo) y que le suba el salario en un 30%
 def aumentarSueldos(empleados,aumento = 0.3):
     for x,y in empleados.items():
-        incremento = y * aumento
-        y += incremento
-        empleados.update({x:y})
-empleados = {
-    "jhonny":200_000, 
-    "bravo":100_000,
-    "Juana":300_000
-}
+        empleados.update({x:y+(y*aumento)})
+empleados = {"jhonny":200_000,"bravo":100_000,"Juana":300_000}
 aumentarSueldos(empleados)
 print(empleados)
