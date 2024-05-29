6093b88fca2aa8c8c83d9913237cd516f1a7a6c4
Endorphyne <ohorodeski@gmail>
2024-05-29 13:59:25 -0300

    Mija trola

--- Practicas2/Exportador_Datos.py
+++ Practicas2/Exportador_Datos.py
@@ -12,8 +12,37 @@ class ClaseFormularioHogar(QMainWindow, Ui_Formulario):#editar "ClaseFormularioH
     #Funcion para extraer los datos del formularios
     def data_Harvest(self):
         datos={}
-        
+        #Datoas de DIRECCION
+        datos.update({"DIRECCION":0})
+        datos.update({"Direccion Principal":self.lnEdit_Direccion_Principal.text()})
+        datos.update({"Direccion Secundaria":self.lnEdit_Direccion_Secundaria.text()})
+        datos.update({"Ciudad":self.comboBx_Ciudad.currentText()})
+        datos.update({"Provincia":self.comboBx_Provincia.currentText()})
+        datos.update({"Codigo Postal":self.lnEdit_Codigo_Postal.text()})
+        #Datos de AMBIENTE
+        datos.update({"AMBIENTE":0})
+        datos.update({"Tipo de Edificio":self.comboBx_Tipo_Edificio.currentText()})
+        datos.update({"Ambientes":self.spnBx_Ambientes.text()})
+        datos.update({"Habitaciones":self.spnBx_Habitaciones.text()})
+        datos.update({"Baños":self.spnBx_Banios.text()})
+        #Datos de CAMAS
+        datos.update({"CAMAS":0})
+        datos.update({"Cama Matrimonial":self.spnBx_Cama_Matrimonial.text()})
+        datos.update({"Cama Sommier":self.spnBx_Cama_Sommier.text()})
+        datos.update({"Cama Simple":self.spnBX_Cama_Simple.text()})
+        datos.update({"Cama Cucheta":self.snpBx_Cama_Cucheta.text()})
+        #Formateo e impresion de los datos
+        for x,y in datos.items():
+            if x == "DIRECCION":
+                print("=============DIRECCION=============")
+            elif x == "AMBIENTE":
+                print("=============AMBIENTE=============")
+            elif x == "CAMAS":
+                print("=============CAMAS=============")
+            else:
+                print(f"{x}:{y}")
 def main():
+
     app = QApplication(sys.argv)
     ventana = ClaseFormularioHogar()
     ventana.show()
